f7a3a0415fb352b12f03a2e9403419a0fe5610b9
wyp6201129@163.com <33771839+7110437wyp@users.noreply.github.com>
2020-02-11 14:08:02 +0800

    Delete mount.sh

--- mount.sh
+++
@@ -1,6 +0,0 @@
-cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
-mount --types proc /proc /mnt/gentoo/proc
-mount --rbind /sys /mnt/gentoo/sys
-mount --make-rslave /mnt/gentoo/sys
-mount --rbind /dev /mnt/gentoo/dev
-mount --make-rslave /mnt/gentoo/dev
